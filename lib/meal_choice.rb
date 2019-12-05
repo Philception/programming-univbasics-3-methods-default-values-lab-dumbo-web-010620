d5a8013dad9df47884920694128dcf8bea02938e
Phillip Henry III <philtrader71@gmail.com>
2019-12-05 19:15:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,6 +6,6 @@ def meal_choice(veg1, veg2, protein = "meat")
     return meal_choice "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("chicken", "carrots", "mashed potatoes")
+meal_choice()
 
 meal_choice("broccoli", "macaroni")
