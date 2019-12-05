343a1dcd6e9af04e3bd6bfcb63da6d7b12bfce88
Phillip Henry III <philtrader71@gmail.com>
2019-12-05 19:14:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(protein= 'meat', veg1, veg2)
+def meal_choice(veg1, veg2, protein = "meat")
   puts "Whether you're omnivorous, vegetarian, or vegan, you're going to be eating a
   nutritious meal!"
   puts "What a nutritious meal!"
